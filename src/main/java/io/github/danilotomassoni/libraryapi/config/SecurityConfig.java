b051b0e2a1906de98e130cc283c3d63895ca1f94
danilotomassoni <daniloaugustotomassoni@gmail.com>
2026-03-01 19:20:48 -0300

    HasRole and HasAuthority.

--- src/main/java/io/github/danilotomassoni/libraryapi/config/SecurityConfig.java
+++ src/main/java/io/github/danilotomassoni/libraryapi/config/SecurityConfig.java
@@ -2,10 +2,17 @@ package io.github.danilotomassoni.libraryapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,17 +20,48 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-        .csrf(AbstractHttpConfigurer::disable)
-        .formLogin(config -> {
-            config.loginPage("/login").permitAll();
-        })
-        //.httpBasic(Customizer.withDefaults())
-        .authorizeHttpRequests(auth -> {
-            auth.anyRequest().authenticated();
-        })
-        .build();
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(config -> {
+                    config.loginPage("/login");
+                })
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/login").permitAll();
+                    auth.requestMatchers(HttpMethod.POST,"/authors/**").hasAuthority("ADD_AUTHOR");
+                    auth.requestMatchers(HttpMethod.POST,"/authors/**").hasRole("ADMIN");
+                    auth.requestMatchers(HttpMethod.DELETE,"/authors/**").hasRole("ADMIN");
+                    auth.requestMatchers(HttpMethod.PUT,"/authors/**").hasRole("ADMIN");
+                    auth.requestMatchers(HttpMethod.GET,"/authors/**").hasAnyRole("USER","ADMIN");
+                    
+                    auth.requestMatchers("/books/**").hasAnyRole("USER","ADMIN");
+                    
+                    auth.anyRequest().authenticated();
+                })
+                .build();
     }
 
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+
+        UserDetails user = User
+                .withUsername("user")
+                .password(encoder.encode("user"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User
+                .withUsername("admin")
+                .password(encoder.encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
 }
